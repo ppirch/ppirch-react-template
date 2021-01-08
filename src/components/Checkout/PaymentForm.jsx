a162a27d7598c27a1a206285276cd75212a9cacf
Pakin Pirch <pakin.pirch@gmail.com>
2021-01-09 00:22:45 +0700

    Clean Code : Checkout

--- src/components/Checkout/PaymentForm.jsx
+++ src/components/Checkout/PaymentForm.jsx
@@ -1,9 +1,11 @@
 import React from "react"
-import Typography from "@material-ui/core/Typography"
-import Grid from "@material-ui/core/Grid"
-import TextField from "@material-ui/core/TextField"
-import FormControlLabel from "@material-ui/core/FormControlLabel"
-import Checkbox from "@material-ui/core/Checkbox"
+import {
+  Typography,
+  Grid,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+} from "@material-ui/core"
 
 export default function PaymentForm() {
   return (
--- src/components/Checkout/Review.jsx
+++ src/components/Checkout/Review.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
-import Typography from "@material-ui/core/Typography"
-import List from "@material-ui/core/List"
-import ListItem from "@material-ui/core/ListItem"
-import ListItemText from "@material-ui/core/ListItemText"
-import Grid from "@material-ui/core/Grid"
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Grid,
+} from "@material-ui/core/Typography"
 
 const products = [
   { name: "Product 1", desc: "A nice thing", price: "$9.99" },
