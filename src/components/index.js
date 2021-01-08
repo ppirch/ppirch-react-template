9ad751ae6e6efbd9c017b5db14f7c06b5b781998
Pakin Pirch <pakin.pirch@gmail.com>
2021-01-09 00:01:13 +0700

    Export Coponents

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+export { default as Copyright } from "./Copyright/Copyright.jsx"
+
+// Home Components
+export { default as ProTip } from "./Home/ProTip.jsx"
+
+// Checkout Components
+export { default as AddressForm } from "./Checkout/AddressForm.jsx"
+export { default as PaymentForm } from "./Checkout/PaymentForm.jsx"
+export { default as Review } from "./Checkout/Review.jsx"
+
+// Blog Components
+export { default as FeaturedPost } from "./Blog/FeaturedPost.jsx"
+export { default as Header } from "./Blog/Header.jsx"
+export { default as Main } from "./Blog/Main.jsx"
+export { default as MainFeaturedPost } from "./Blog/MainFeaturedPost.jsx"
+export { default as Markdown } from "./Blog/Markdown.jsx"
+export { default as Sidebar } from "./Blog/Sidebar.jsx"
+export { default as Footer } from "./Blog/Footer.jsx"
+
+// Dashboard
+export { mainListItems, secondaryListItems } from "./Dashboard/listItems.jsx"
+export { default as Chart } from "./Dashboard/Chart.jsx"
+export { default as Deposits } from "./Dashboard/Deposits.jsx"
+export { default as Orders } from "./Dashboard/Orders.jsx"
+export { default as Title } from "./Dashboard/Title.jsx"
