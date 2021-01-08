5c28774387febfaf2eebec3753637e0f91dd85ec
Pakin Pirch <pakin.pirch@gmail.com>
2021-01-09 00:32:24 +0700

    Clean Code : Album

--- src/pages/Album/Album.jsx
+++ src/pages/Album/Album.jsx
@@ -1,18 +1,18 @@
 import React from "react"
-import AppBar from "@material-ui/core/AppBar"
-import Button from "@material-ui/core/Button"
-import CameraIcon from "@material-ui/icons/PhotoCamera"
-import Card from "@material-ui/core/Card"
-import CardActions from "@material-ui/core/CardActions"
-import CardContent from "@material-ui/core/CardContent"
-import CardMedia from "@material-ui/core/CardMedia"
-import CssBaseline from "@material-ui/core/CssBaseline"
-import Grid from "@material-ui/core/Grid"
-import Toolbar from "@material-ui/core/Toolbar"
-import Typography from "@material-ui/core/Typography"
+import {
+  AppBar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Toolbar,
+  Typography,
+  Container,
+} from "@material-ui/core"
+import { Camera as CameraIcon } from "@material-ui/icons"
 import { makeStyles } from "@material-ui/core/styles"
-import Container from "@material-ui/core/Container"
-import Link from "@material-ui/core/Link"
 import { Copyright } from "../../components/"
 
 const useStyles = makeStyles((theme) => ({
