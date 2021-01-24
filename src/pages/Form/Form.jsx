7c1131c6ef686c716ab01248f793dd6c1b0cf72c
ppirch <pakin.pirch@gmail.com>
2021-01-24 11:29:37 +0700

    Implement Form Page

---
+++ src/pages/Form/Form.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react"
+import { Link as LinkDOM } from "react-router-dom"
+import { Container, Typography, Box, Button } from "@material-ui/core"
+import { Copyright, ProTip, MenuPopup, Form } from "../../components"
+import { useContent } from "../../utils/machine"
+import { useMachine } from "@xstate/react"
+import ReactJson from "react-json-view"
+
+const setPub = async (sub) => {
+  await new Promise((resolve) => setTimeout(resolve, 500))
+  return { hello: "world" }
+}
+
+const Content = (props) => {
+  const { sub, redirect } = props
+  let stateParser
+  const [current, send] = useMachine(useContent, {
+    services: {
+      fetchData: () =>
+        setPub(sub).then((data) => {
+          return {
+            data,
+          }
+        }),
+    },
+  })
+
+  useEffect(() => {
+    send("RETRY")
+  }, [send, sub])
+
+  switch (current.value) {
+    case "idle":
+      return <h1>Blank</h1>
+    case "loading":
+      return <h1>Loading...</h1>
+    case "success":
+      stateParser = current.context.data
+      const { data } = stateParser
+      return (
+        <>
+          <Box my={4}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Create React App v4-beta example
+            </Typography>
+            <Form />
+            <ProTip />
+            <Copyright />
+          </Box>
+        </>
+      )
+    case "failure":
+      return <h1>Reload</h1>
+    default:
+      return null
+  }
+}
+
+export default function App(props) {
+  const { history } = props
+  const redirect = () => {
+    history.push("/album")
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Content sub={{}} redirect={redirect} />
+    </Container>
+  )
+}
