7b8a8a3712f4f6f40fd07f4ac5adb4e6e34c2f9d
ppirch <pakin.pirch@gmail.com>
2021-07-14 12:54:09 +0700

    use React Query

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,42 +2,27 @@ import React, { useEffect } from "react"
 import { Link as LinkDOM } from "react-router-dom"
 import { Container, Typography, Box, Button } from "@material-ui/core"
 import { Copyright, ProTip, MenuPopup } from "../../components"
-import { useContent } from "../../utils/machine"
-import { useMachine } from "@xstate/react"
 import ReactJson from "react-json-view"
+import { useQuery } from 'react-query'
 
 const setPub = async (sub) => {
   await new Promise((resolve) => setTimeout(resolve, 500))
   return { hello: "world" }
 }
 
-const Content = (props) => {
-  const { sub, redirect } = props
-  let stateParser
-  const [current, send] = useMachine(useContent, {
-    services: {
-      fetchData: () =>
-        setPub(sub).then((data) => {
-          return {
-            data,
-          }
-        }),
-    },
-  })
+export default function Home(props) {
+  const { history } = props
+  const redirect = () => {
+    history.push('/album')
+  }
+  const { isLoading, error, data } = useQuery('Data', () => setPub())
+ 
+  if (isLoading) return 'Loading...'
+ 
+  if (error) return 'An error has occurred: ' + error.message
 
-  useEffect(() => {
-    send("RETRY")
-  }, [send, sub])
-
-  switch (current.value) {
-    case "idle":
-      return <h1>Blank</h1>
-    case "loading":
-      return <h1>Loading...</h1>
-    case "success":
-      stateParser = current.context.data
-      const { data } = stateParser
-      return (
+  return (
+    <Container maxWidth="sm">
         <>
           <Box my={4}>
             <Typography variant="h4" component="h1" gutterBottom>
@@ -55,23 +40,6 @@ const Content = (props) => {
             <Copyright />
           </Box>
         </>
-      )
-    case "failure":
-      return <h1>Reload</h1>
-    default:
-      return null
-  }
-}
-
-export default function App(props) {
-  const { history } = props
-  const redirect = () => {
-    history.push("/album")
-  }
-
-  return (
-    <Container maxWidth="sm">
-      <Content sub={{}} redirect={redirect} />
     </Container>
   )
 }
--- src/pages/ShowImage/ShowImage.jsx
+++ src/pages/ShowImage/ShowImage.jsx
@@ -1,66 +1,35 @@
 import React, { useEffect } from "react"
 import { Container, Typography, Box } from "@material-ui/core"
 import { Copyright, ProTip } from "../../components"
-import { useContent } from "../../utils/machine"
-import { useMachine } from "@xstate/react"
 import { getCatAPI } from "../../api/cat-api"
 import Image from "material-ui-image"
+import { useQuery } from 'react-query'
 
-const Content = (props) => {
-  const { sub } = props
-  let stateParser
-  const [current, send] = useMachine(useContent, {
-    services: {
-      fetchData: () =>
-        getCatAPI().then((data) => {
-          return {
-            data,
-          }
-        }),
-    },
-  })
-
-  useEffect(() => {
-    send("RETRY")
-  }, [send, sub])
-
-  switch (current.value) {
-    case "idle":
-      return <h1>Blank</h1>
-    case "loading":
-      return <h1>Loading...</h1>
-    case "success":
-      stateParser = current.context.data
-      const { data } = stateParser
-      const { image, fact } = data
-      return (
-        <>
-          <Image src={image.file} style={{ margin: 10 }} />
-          <Typography>Cat Fact : {fact.text}</Typography>
-        </>
-      )
-    case "failure":
-      return <h1>Reload</h1>
-    default:
-      return null
-  }
-}
 
 const ShowImage = (props) => {
   const { history } = props
 
+  const { isLoading, error, data } = useQuery('catData', () => getCatAPI().then())
+
+  if (isLoading) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
+  const { image, fact } = data
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Cat Image And Random Fact
         </Typography>
-        <Content sub={{}} />
+        <>
+          <Image src={image.file} style={{ margin: 10 }} />
+          <Typography>Cat Fact : {fact.fact}</Typography>
+        </>
         <ProTip />
         <Copyright />
       </Box>
     </Container>
   )
 }
-
 export default ShowImage
